cb24dd6666897138d502a5077d0369b132ee98d5
Sahand <Sahandhosseinii@gmail.com>
2018-01-27 23:00:24 -0800

    Imported operator and functools to use an ncr method I found at:https://stackoverflow.com/questions/4941753/is-there-a-math-ncr-function-in-python
    Added Binomial Variable method.

--- Model/RandomVariable.py
+++ Model/RandomVariable.py
@@ -1,3 +1,12 @@
+import operator as op
+import functools
+def ncr(n, r):
+    r = min(r, n-r)
+    if r == 0: return 1
+    numer = functools.reduce(op.mul, range(n, n-r, -1))
+    denom = functools.reduce(op.mul, range(1, r+1))
+    return numer//denom
+
 class RandomVariable(object):
         def __init__(self,type):
             self.type = type
@@ -23,4 +32,15 @@ def BernVar(p):
     return x
 
 X = BernVar(1/2)
+print(X)
+
+'''
+Method to create a binomial random var and assign a PMF to it
+uses the ncr method copied from https://stackoverflow.com/questions/4941753/is-there-a-math-ncr-function-in-python
+'''
+def BinVar(n,p):
+    x = {i: ((ncr(n,i))*(p**i)*((1-p)**(n-i))) for i in range(n+1)}
+    return x
+
+X = BinVar(4,0.5)
 print(X)
